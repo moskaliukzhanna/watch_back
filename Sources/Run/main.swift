3d2d259f7eb1a8800d22736d9d53c0d31c6f732a
Zhanna Moskaliuk <octoberrock11@gmail.com>
2022-02-17 10:21:35 +0200

    Add server address configuration

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -4,6 +4,8 @@ import Vapor
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
+//app.http.server.configuration.hostname = "192.168.1.3"
+//app.http.server.configuration.port = 8080
 defer { app.shutdown() }
 try configure(app)
 try app.run()
